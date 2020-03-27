7cc4169516efbce0a1567e25eb85110cc84b100c
AH-Toby <1751001928@qq.com>
2020-03-27 16:50:46 +0800

    创建子应用

--- DjangoBasics/settings.py
+++ DjangoBasics/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    """
+    index视图
+    :param request: 包含了请求信息的请求对象
+    :return: 响应对象
+    """
+
+    return HttpResponse("hello the world")
